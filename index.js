e0b5c67d44c8c65e398ce61477edbcc2de9ad71f
Allan Henrique Moreira Rodrigues <allanhenrique@gmail.com>
2018-05-18 15:51:02 -0300

    implementando crupie

--- index.js
+++ index.js
@@ -1,30 +1,50 @@
-const cards = ['Ace',
-    'two',
-    'three',
-    'four',
-    'five',
-    'six',
-    'seven',
-    'eight',
-    'nine',
-    'ten',
-    'Joker',
-    'Queen',
-    'King'
+const cards = [{ 1: 'Ace' },
+    { 2: 'Two' },
+    { 3: 'Three' },
+    { 4: 'Four' },
+    { 5: 'Five' },
+    { 6: 'Six' },
+    { 7: 'Seven' },
+    { 8: 'Eight' },
+    { 9: 'Nine' },
+    { 10: 'Ten' },
+    { 10: 'Joker' },
+    { 10: 'Queen' },
+    { 10: 'King' }
 ];
 
 const suits = ['Spades', 'Hearts', 'Diamonds', 'Clubs'];
 
-let deck = [];
-let index = 0;
 
-function getOneMore() {
+let deck = [];
+let deckDealer = [];
+// let index = 0;
+
+function hit() {
+    getOneMore(deck);
+}
+
+function stand() {
+    getOneMore(deckDealer);
+}
+
+function countCards() {
+    deck.array.forEach(element => {
+        if (element === 'Ace' & deck.includes('King' || 'Queen' || 'Joker')) {
+
+        } else {
+
+        }
+    });
+}
+
+function getOneMore(obj) {
     let sortedCard = sortCard();
 
-    if (!deck.includes(sortedCard)) {
-        deck.push(sortedCard);
-        console.log(deck, sortedCard);
-        showDeck(deck, deck.length - 1);
+    if (!obj.includes(sortedCard)) {
+        obj.push(sortedCard);
+        console.log(obj, sortedCard);
+        showDeck(obj, obj.length - 1);
     } else {
         getOneMore();
     }
@@ -37,14 +57,15 @@ function getOneMore() {
     }
 }
 
-function showDeck(deck, index) {
+function showDeck(obj, index) {
     let buildHtml = document.getElementById('Deck');
-    const element = '<li> ' + deck[index] + '</li>';
+    const element = '<li> ' + obj[index] + '</li>';
     buildHtml.innerHTML = buildHtml.innerHTML.concat(element);
 }
 
 function newGame() {
     deck = [];
+    deckDealer = [];
     let buildHtml = document.getElementById('Deck');
     buildHtml.innerHTML = 'Your card\'s:';
 }
