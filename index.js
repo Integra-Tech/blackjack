2360a408735e2d0902f432b4e2ad1ba820982f88
allanhenriquers <allanhenrique@gmail.com>
2018-05-19 17:11:18 -0300

    Colorindo a Poha toda

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
-const cards = [{ 'value': 1, 'name': 'Ace' },
-                {'value': 2,  'name': 'Two' },
-                {'value': 3,  'name': 'Three' },
-                {'value': 4,  'name': 'Four' },
-                {'value': 5,  'name': 'Five' },
-                {'value': 6,  'name': 'Six' },
-                {'value': 7,  'name': 'Seven' },
-                {'value': 8,  'name': 'Eight' },
-                {'value': 9,  'name': 'Nine' },
-                {'value': 10, 'name': 'Ten' },
-                {'value': 10, 'name': 'Joker' },
-                {'value': 10, 'name': 'Queen' },
-                {'value': 10, 'name': 'King' }
+const cards = [
+    { 'value': 1, 'name': 'Ace', 'visual': 'A'},
+    {'value': 2,  'name': 'Two', 'visual': '2'},
+    {'value': 3,  'name': 'Three', 'visual': '3'},
+    {'value': 4,  'name': 'Four', 'visual': '4'},
+    {'value': 5,  'name': 'Five', 'visual': '5'},
+    {'value': 6,  'name': 'Six', 'visual': '6'},
+    {'value': 7,  'name': 'Seven', 'visual': '7'},
+    {'value': 8,  'name': 'Eight', 'visual': '8'},
+    {'value': 9,  'name': 'Nine', 'visual': '9'},
+    {'value': 10, 'name': 'Ten', 'visual': '10'},
+    {'value': 10, 'name': 'Joker', 'visual': 'J'},
+    {'value': 10, 'name': 'Queen', 'visual': 'Q'},
+    {'value': 10, 'name': 'King', 'visual': 'K'}
 ];
 
-const suits = ['Spades', 'Hearts', 'Diamonds', 'Clubs'];
+const suits = [
+    'Spades',
+    'Hearts',
+    'Diamonds',
+    'Clubs'
+];
 
 
 let deck = [];
 let deckDealer = [];
-// let index = 0;
 
 function hit() {
     getOneMore(deck);
@@ -28,39 +33,40 @@ function stand() {
     getOneMore(deckDealer);
 }
 
-function countCards() {
-    deck.array.forEach(element => {
-        if (element === 'Ace' & deck.includes('King' || 'Queen' || 'Joker')) {
-
-        } else {
-
-        }
-    });
-}
-
 function getOneMore(obj) {
     let sortedCard = sortCard();
 
     if (!obj.includes(sortedCard)) {
         obj.push(sortedCard);
         console.log(obj, sortedCard);
-        showDeck(obj, obj.length - 1);
+        showDeck(obj);
     } else {
         getOneMore(obj);
     }
 
     function sortCard() {
-        let card = cards[Math.floor(Math.random() * cards.length )].name;
-        let suit = suits[Math.floor(Math.random() * suits.length )];
-        let sortedCard = card + ' of ' + suit;
-        return sortedCard;
+        return {
+            'name': cards[Math.floor(Math.random() * cards.length )].visual,
+            'suit': suits[Math.floor(Math.random() * suits.length )]  
+        }
     }
 }
 
-function showDeck(obj, index) {
+function showDeck(obj) {
+    const suitClass = {
+        'Spades': {'visual':'♠', 'class': 'black'},
+        'Hearts': {'visual':'♥', 'class': 'red'},
+        'Diamonds': {'visual':'♦', 'class': 'red'},
+        'Clubs': {'visual':'♣', 'class': 'black'}
+    };
     let buildHtml = document.getElementById('Deck');
-    const element = '<li> ' + obj[index] + '</li>';
-    buildHtml.innerHTML = buildHtml.innerHTML.concat(element);
+    buildHtml.innerHTML = '';
+    
+    obj.forEach(card => {
+        buildHtml.innerHTML += '<div class="' + suitClass[card.suit].class + '">'
+        +'<div class="number">' + card.name + '</div>'
+        +'<div class="suit">' + suitClass[card.suit].visual + '</div><div>';
+    });
 }
 
 function newGame() {
