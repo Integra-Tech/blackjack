96b3513e74afa1ee92a7012f915a789b22a38d94
Allan Henrique Moreira Rodrigues <allanhenrique@gmail.com>
2018-05-20 11:32:44 -0300

    dell

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const cards = [{ 1: 'Ace' },
-    { 2: 'Two' },
-    { 3: 'Three' },
-    { 4: 'Four' },
-    { 5: 'Five' },
-    { 6: 'Six' },
-    { 7: 'Seven' },
-    { 8: 'Eight' },
-    { 9: 'Nine' },
-    { 10: 'Ten' },
-    { 10: 'Joker' },
-    { 10: 'Queen' },
-    { 10: 'King' }
+const cards = [{ 'value': 1, 'name': 'Ace' },
+    { 'value': 2, 'name': 'Two' },
+    { 'value': 3, 'name': 'Three' },
+    { 'value': 4, 'name': 'Four' },
+    { 'value': 5, 'name': 'Five' },
+    { 'value': 6, 'name': 'Six' },
+    { 'value': 7, 'name': 'Seven' },
+    { 'value': 8, 'name': 'Eight' },
+    { 'value': 9, 'name': 'Nine' },
+    { 'value': 10, 'name': 'Ten' },
+    { 'value': 10, 'name': 'Joker' },
+    { 'value': 10, 'name': 'Queen' },
+    { 'value': 10, 'name': 'King' }
 ];
 
 const suits = ['Spades', 'Hearts', 'Diamonds', 'Clubs'];
@@ -21,44 +21,47 @@ let deckDealer = [];
 // let index = 0;
 
 function hit() {
-    getOneMore(deck);
+    getOneMore(deck, 'deck');
 }
 
 function stand() {
-    getOneMore(deckDealer);
+    getOneMore(deckDealer, 'deckDealer');
 }
+
+let playerPoints = 0;
 
 function countCards() {
     deck.array.forEach(element => {
         if (element === 'Ace' & deck.includes('King' || 'Queen' || 'Joker')) {
-
+            playerPoints = 10
         } else {
-
+            playerPoints += element.value;
         }
+        console.log(playerPoints);
     });
 }
 
-function getOneMore(obj) {
+function getOneMore(obj, divId) {
     let sortedCard = sortCard();
 
     if (!obj.includes(sortedCard)) {
         obj.push(sortedCard);
         console.log(obj, sortedCard);
-        showDeck(obj, obj.length - 1);
+        showDeck(obj, obj.length - 1, divId);
     } else {
         getOneMore();
     }
 
     function sortCard() {
-        let card = cards[Math.floor(Math.random() * cards.length >> 1)];
+        let card = cards[Math.floor(Math.random() * cards.length >> 1)].name;
         let suit = suits[Math.floor(Math.random() * suits.length >> 1)];
         let sortedCard = card + ' of ' + suit;
         return sortedCard;
     }
 }
 
-function showDeck(obj, index) {
-    let buildHtml = document.getElementById('Deck');
+function showDeck(obj, index, divId) {
+    let buildHtml = document.getElementById(divId);
     const element = '<li> ' + obj[index] + '</li>';
     buildHtml.innerHTML = buildHtml.innerHTML.concat(element);
 }
@@ -66,6 +69,8 @@ function showDeck(obj, index) {
 function newGame() {
     deck = [];
     deckDealer = [];
-    let buildHtml = document.getElementById('Deck');
-    buildHtml.innerHTML = 'Your card\'s:';
+    var buildHtml = document.getElementById('deck');
+    var htmlDealer = document.getElementById('deckDealer')
+    buildHtml.innerHTML = '';
+    htmlDealer.innerHTML = '';
 }
